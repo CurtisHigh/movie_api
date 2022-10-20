4a79a31038e0e004e54bc9fdb22d3edec8208f0a
CurtisHigh <105828905+CurtisHigh@users.noreply.github.com>
2022-10-19 21:44:10 -0500

    added morgan, express.static(), errorhandler

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-const express = require('express');
+const express = require('express')
+lodash = require('lodash')
+bodyParser = require('body-parser')
+fs = require('fs')
+path = require('path')
+morgan = require('morgan');
+
 const app = express();
 
-const lodash = require('lodash'),
-  bodyParser = require('body-parser');
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+let movies = [
+  {
+    title: 'Avatar'
+  },
+  {
+    title: 'Lord of the Rings'
+  },
+  {
+    title: 'Star Wars'
+  }
+];
+
+app.use(morgan('combined', {stream: accessLogStream}));
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('An error has occured');
+});
+
+app.use('/public', express.static('./public'));
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});

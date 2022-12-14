284e5590d8636f8381a0273e14fa4696af8dc733
CurtisHigh <105828905+CurtisHigh@users.noreply.github.com>
2022-12-14 15:48:13 -0600

    adding request methods for endpoints

--- index.js
+++ index.js
@@ -11,17 +11,42 @@ const app = express();
 // a ‘log.txt’ file is created in root directory
 //const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-let movies = [
-  {
-    title: 'Avatar'
-  },
-  {
-    title: 'Lord of the Rings'
-  },
-  {
-    title: 'Star Wars'
-  }
-];
+app.get('/movies', (req, res) => {
+  res.send('list of movies');
+});
+
+app.get('/movies/:title', (req, res) => {
+  res.send("A JSON object containing data about the provided movie title including the description, genre, director, image URL, whether it's featured or not");
+});
+
+app.get('/movies/genre/:genreName', (req,res) => {
+  res.send('A JSON object containing information about the specified genre name');
+});
+
+app.get('/movies/directors/:directorName', (req,res) => {
+  res.send('A JSON object containing data about a director (bio, birth year, death year) by name');
+});
+
+app.post('/users', (req,res) => {
+  res.send('A JSON object containing the users name and a generated id {"name": "usersName", "id": "randomID"}');
+});
+
+app.put('/users/:id', (req,res) => {
+  res.send('A JSON object containing the updated user info {"name": "updatedUserName", "id":"userId"}');
+});
+
+app.post('/users/:id/:movieTitle', (req,res) => {
+  res.send('A message stating that "movieTitle" has been added to "userId\'s" list of favorites');
+});
+
+app.delete('/users/:id/:movieTitle', (req,res) => {
+  res.send('A message stating that "movieTitle" has been deleted from "userId\'s" list of favorites');
+});
+
+app.delete('/users/:id', (req,res) => {
+  res.send('A massage stating that users email has been removed from the list of users');
+});
+
 
 //log to txt file
 //app.use(morgan('combined', {stream: accessLogStream}));
@@ -36,10 +61,7 @@ app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-app.get('/movies', (req, res) => {
-  res.json(movies);
-});
-
+//error handling middleware function
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('An error has occured');

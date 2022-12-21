46d53eddae19e0e2ec4bfde78ba48339a0d16e6b
CurtisHigh <105828905+CurtisHigh@users.noreply.github.com>
2022-12-21 11:30:25 -0600

    implement express-validator

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ morgan = require('morgan')
 mongoose = require('mongoose')
 Models = require('./models.js');
 
+const {check, validationResult} = require('express-validator');
+
 const app = express();
 
 //body-parser middleware, must be above any other endpoint middleware
@@ -132,7 +134,22 @@ app.get('/movies/directors/:directorName', passport.authenticate('jwt', {session
 });
 
 //POST a new user
-app.post('/users', (req,res) => {
+app.post('/users', 
+[
+  check('Username', 'Username is required').isLength({min:5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+],
+(req,res) => {
+  
+  // check validation object for errors
+  let errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()});
+  }
+
   let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({Username: req.body.Username})
   .then((user) => {
@@ -161,7 +178,22 @@ app.post('/users', (req,res) => {
 });
 
 //Allow users to update account info
-app.put('/users/:Username', passport.authenticate('jwt', {session: false}), (req,res) => {
+app.put('/users/:Username', passport.authenticate('jwt', {session: false}), 
+[
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowd').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+],
+(req,res) => {
+
+  // check validation object for errors
+  let errors = validationResult(req);
+  
+  if(!errors.isEmpty()) {
+    return res.status(422).json({errors: errors.array()});
+  }
+
   let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOneAndUpdate({Username: req.params.Username}, {
     $set: {
